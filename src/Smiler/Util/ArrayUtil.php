35e8372e01a945cd1c10fc28c1b8bbeb9274446b
fuyou <fuyou@addnewer.com>
2017-05-15 17:07:58 +0800

    添加过滤数组空置的函数

--- src/Smiler/Util/ArrayUtil.php
+++ src/Smiler/Util/ArrayUtil.php
@@ -1,5 +1,18 @@
 <?php
 namespace Smiler\Util;
-class ArrayUtil{
-    
+
+class ArrayUtil
+{
+
+    public static function filterNullVal($arr)
+    {
+        $newArr = [];
+        foreach ($arr as $key => $val) {
+            if ($val === null || $val === '') {
+                continue;
+            }
+            $newArr[$key] = $val;
+        }
+        return $newArr;
+    }
 }
