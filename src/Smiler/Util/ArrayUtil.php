e3d59bef50a17c9621ebd35c029ee8ebd1f4b7a3
fuyou <fuyou@addnewer.com>
2017-05-19 11:48:24 +0800

    修改filterNullVal函数的实现

--- src/Smiler/Util/ArrayUtil.php
+++ src/Smiler/Util/ArrayUtil.php
@@ -6,13 +6,11 @@ class ArrayUtil
 
     public static function filterNullVal($arr)
     {
-        $newArr = [];
         foreach ($arr as $key => $val) {
             if ($val === null || $val === '') {
-                continue;
+               unset($arr[$key]);
             }
-            $newArr[$key] = $val;
         }
-        return $newArr;
+        return $arr;
     }
 }
